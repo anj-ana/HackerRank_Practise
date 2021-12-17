cd39e23296175e1d7cdde1ae9aafb20516b41cf5
Anjana <66191342+anj-ana@users.noreply.github.com>
2021-12-17 23:21:40 +0530

    first commit

---
+++ Java/Practise2.java
@@ -0,0 +1,22 @@
+
+        int a = 12;
+        double dou = 5.0;
+        String s1 = "is a coding platform";
+        
+
+        /* Read and save an integer, double, and String to your variables.*/
+        a = scan.nextInt();
+        dou = scan.nextDouble();
+        scan.nextLine();
+        s1 = scan.nextLine();
+
+        /* Print the sum of both integer variables on a new line. */
+        System.out.println(i+a);
+        System.out.println(d+dou);
+        System.out.println(s+s1);
+
+        /* Print the sum of the double variables on a new line. */
+		
+        /* Concatenate and print the String variables on a new line;
+        	the 's' variable above should be printed first. */
+
