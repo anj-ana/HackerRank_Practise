70e29d16562ecfaf264d264249e95234429e42c0
Anjana <66191342+anj-ana@users.noreply.github.com>
2021-12-17 23:28:40 +0530

    first commit

---
+++ Java/practise4.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+
+     
